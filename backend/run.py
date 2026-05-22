92fcd77861b5dd8aa1aa1281c3b291c5f5eca0f1
shilpa3030 <shilpadinda89@gmail.com>
2026-05-22 19:49:26 +0530

    fixed requirements location

--- backend/run.py
+++ backend/run.py
@@ -59,4 +59,5 @@ with app.app_context():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    # app.run(host="0.0.0.0", port=port)
     socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
